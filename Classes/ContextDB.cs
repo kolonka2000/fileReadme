06c8133ef7482973de6307eb6f6ec03f76b48221
kolonka2000 <102492626+kolonka2000@users.noreply.github.com>
2022-04-19 07:09:07 +0500

    Add files via upload

---
+++ Classes/ContextDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM04
+{
+    internal class ContextDB
+    {
+        private static PM04.TheBestDBEntities _context { get; set; }
+
+        public static PM04.TheBestDBEntities GetContext()
+        {
+            if (_context == null)
+                _context = new PM04.TheBestDBEntities();
+            return _context;
+        }
+    }
+}
